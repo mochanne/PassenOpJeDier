061138a905dd7fb7db7aaf48a3f3a071d5bdaa50
anne <mocchapi@gmail.com>
2022-04-06 05:13:13 +0200

    switch blades to extend a default page

--- resources/views/user/item.blade.php
+++ resources/views/user/item.blade.php
@@ -1,18 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/main.css">
-    <title>{{ ucfirst($user->fname) }}'s profile</title>
-</head>
-<body>
+@extends('default')
+
+@section('title')
+    {{$user->fname}}'s page
+@endsection
+
+@section('content')
     <h1>{{$user->fname}} {{$user->lname}}</h1>
     <img src="{{$user->avatar}}" alt="profile picture"/>
+
+    @if ($user->blocked)
+        <h3>This user has been banned.</h3>
+    @endif
 
     @if ($user->home_id)
         <a href="/homes/{{ $user->home_id }}">go to home</a>
     @endif
-</body>
-</html>
+
+@endsection
